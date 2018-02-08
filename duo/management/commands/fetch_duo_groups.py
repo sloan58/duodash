0d8044625534e9abd6ab058b3a444adca048177c
Martin Sloan <martin.sloan@karma-tek.com>
2018-02-07 23:06:57 -0500

    add duo group model and sync command

--- duo/management/commands/fetch_duo_groups.py
+++ duo/management/commands/fetch_duo_groups.py
@@ -1,14 +1,12 @@
-import pytz
-import datetime
 import duo_client
 from django.core.management.base import BaseCommand, CommandError
 
-from duo.models import User
+from duo.models import Group
 
 
 class Command(BaseCommand):
 
-    help = 'Fetch all Duo Users via Admin API'
+    help = 'Fetch all Duo Groups via Admin API'
 
     def handle(self, *args, **options):
 
@@ -24,50 +22,40 @@ class Command(BaseCommand):
 
         self.stdout.write(self.style.WARNING('[-]') + ' Creating Duo Admin Client and querying the API...')
 
-        # Fetch all Duo Users
+        # Fetch all Duo Groups
         try:
-            users = admin_api.get_users()
+            groups = admin_api.get_groups()
         except RuntimeError as e:
             self.stdout.write(self.style.ERROR('[!] %s (%s)' % (e, type(e))))
             exit()
 
-        self.stdout.write(self.style.WARNING('[-]') + ' Found %s Duo Users to store locally' % len(users))
+        self.stdout.write(self.style.WARNING('[-]') + ' Found %s Duo Groups to store locally' % len(groups))
 
-        # Just picking a timezone since we have to....
-        timezone = pytz.timezone("America/New_York")
+        # Iterate the Groups for insert/update
+        for group in groups:
 
-        # Iterate the Users for insert/update
-        for user in users:
-
-            # Django model DateTimeField does not play nice
-            # with Unix Timestamps.  Check to see if it exists
-            # and convert it to a Datetime format with timezone
-            if user['last_login'] is not None:
-                last_login = datetime.datetime.fromtimestamp(user['last_login'], tz=timezone)
-            else:
-                last_login = None
-
-            # Create a dictionary for the Duo User
-            duo_user = {
-                'user_id': user['user_id'],
-                'username': user['username'],
-                'email': user['email'],
-                'status': user['status'],
-                'realname': user['realname'],
-                'notes': user['notes'],
-                'last_login': last_login
+            # Create a dictionary for the Duo Group
+            duo_group = {
+                'group_id': group['group_id'],
+                'name': group['name'],
+                'desc': group['desc'],
+                'status': group['status'],
+                'mobile_otp_enabled': group['mobile_otp_enabled'],
+                'push_enabled': group['push_enabled'],
+                'sms_enabled': group['sms_enabled'],
+                'voice_enabled': group['voice_enabled'],
             }
 
-            # Call get_or_create with the duo_user dictionary
+            # Call get_or_create with the duo_group dictionary
             try:
-                instance, created = User.objects.get_or_create(user_id=user['user_id'], defaults=duo_user)
+                instance, created = Group.objects.get_or_create(group_id=group['group_id'], defaults=duo_group)
             except Exception as e:
                 self.stdout.write(self.style.ERROR('[!] %s (%s)' % (e, type(e))))
                 continue
 
             # If the object was not 'created', then it already existed.  Update the model as needed
             if not created:
-                for attr, value in duo_user.items():
+                for attr, value in duo_group.items():
                     setattr(instance, attr, value)
 
                 # Save the updates
--- duo/models.py
+++ duo/models.py
@@ -10,3 +10,15 @@ class User(models.Model):
     realname = models.CharField(max_length=200, null=True)
     notes = models.CharField(max_length=200)
     last_login = models.DateTimeField('last login', null=True)
+
+
+# Duo Group model
+class Group(models.Model):
+    group_id = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
+    mobile_otp_enabled = models.BooleanField(default=False)
+    push_enabled = models.BooleanField(default=False)
+    sms_enabled = models.BooleanField(default=False)
+    voice_enabled = models.BooleanField(default=False)
